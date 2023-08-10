8ff1c01f4fe03d66c2e1288714f859640a950794
Kennie Davis <kenniejaydavis@gmail.com>
2023-08-10 15:07:01 -0700

    style(ghostbot): code formatting

--- apps/ghostbot/src/app/(tabs)/index.tsx
+++ apps/ghostbot/src/app/(tabs)/index.tsx
@@ -1,4 +1,3 @@
-
 import { Tabs } from 'expo-router';
 import React from 'react';
 import FontAwesome from '@expo/vector-icons/FontAwesome';
@@ -6,20 +5,28 @@ import FontAwesome from '@expo/vector-icons/FontAwesome';
 import { View, Text } from 'react-native';
 
 /* eslint-disable-next-line */
-export interface TodayProps {
-}
-
+export interface TodayProps {}
 
 export function Today(props: TodayProps) {
   return (
     <>
-    <Tabs.Screen options={{ title: 'Today', tabBarIcon: ({ color }) => <FontAwesome color={color} size={24} name='comments' />}}/>
-    <View>
-      <Text>Welcome to today!</Text>
-    </View>
+      <Tabs.Screen
+        options={{
+          title: 'Today',
+          tabBarIcon: ({ color, focused }) => (
+            <FontAwesome
+              color={color}
+              size={24}
+              name={focused ? 'comments' : 'comments-o'}
+            />
+          ),
+        }}
+      />
+      <View>
+        <Text>Welcome to today!</Text>
+      </View>
     </>
   );
-};
-
+}
 
 export default Today;
