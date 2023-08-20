7b55924ab39d3189df08801eb3d30bb7e7cdbadc
Kennie Davis <kenniejaydavis@gmail.com>
2023-08-20 15:11:16 -0700

    chore(ghostbot): simple login form adjustments

--- libs/ghostbot/feature-admin/src/components/authentication-form.tsx
+++ libs/ghostbot/feature-admin/src/components/authentication-form.tsx
@@ -1,6 +1,13 @@
-import { useCallback, useState } from 'react';
-import { Pressable, Text, TextInput, View } from 'react-native';
+import { ReactNode, useCallback, useState } from 'react';
+import {
+  KeyboardAvoidingView,
+  Pressable,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
 import Realm from 'realm';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export type AuthenticationCallback = (
   credentials: Realm.Credentials<SimpleObject>
@@ -12,18 +19,16 @@ export type AuthenticationModeCallback = (mode: AuthenticationMode) => void;
 export type AuthenticationFormProps = {
   mode?: AuthenticationMode;
   onAuthenticate?: AuthenticationCallback;
-  onChangeMode?: AuthenticationModeCallback;
+  onToggleMode?: AuthenticationModeCallback;
 };
 
 export function AuthenticationForm({
   mode = 'login',
   onAuthenticate,
-  onChangeMode,
+  onToggleMode,
 }: AuthenticationFormProps) {
   const [email, setEmail] = useState<string>();
   const [password, setPassword] = useState<string>();
-
-  // const [mode, setMode] = useState<AuthenticationMode>(initialMode);
 
   const handleAuthenticate = useCallback(() => {
     if (email && password) {
@@ -31,61 +36,88 @@ export function AuthenticationForm({
     }
   }, [email, onAuthenticate, password]);
 
-  const handleChangeMode = useCallback(() => {
-    onChangeMode?.(mode === 'login' ? 'register' : 'login');
-  }, [mode, onChangeMode]);
+  const handleToggleMode = useCallback(() => {
+    onToggleMode?.(mode === 'login' ? 'register' : 'login');
+  }, [mode, onToggleMode]);
 
   return (
-    <View className="w-2/3 self-center gap-2">
-      <Text>{mode === 'login' ? 'LOGIN' : 'REGISTER'}</Text>
-      <View className="border-hairline border-slate-500 p-2 rounded-md">
-        <TextInput
-          keyboardType="email-address"
-          onChangeText={setEmail}
-          placeholder="Email Address"
-          autoCapitalize="none"
-          autoCorrect={false}
-        />
-      </View>
-      <View className="border-hairline border-slate-500 p-2 rounded-md">
-        <TextInput
-          onChangeText={setPassword}
-          placeholder="Password"
-          secureTextEntry
-          autoCapitalize="none"
-          autoCorrect={false}
-        />
-      </View>
-      <Pressable
-        className={`items-center justify-center p-3 rounded-md ${
-          !email || !password ? 'bg-neutral-300' : 'bg-amber-500'
-        }`}
-        disabled={!email || !password}
-        onPress={handleAuthenticate}
-        // onPress={async () => {
-        //   if (email && password) {
-        //     if (mode === 'register') {
-        //       await app.emailPasswordAuth.registerUser({ email, password });
-        //     }
-        //     await app.logIn(Realm.Credentials.emailPassword(email, password));
-        //   }
-        // }}
-      >
-        <Text className="font-bold text-white uppercase">
-          {mode === 'login' ? 'Login' : 'Register'}
-        </Text>
-      </Pressable>
-      <Pressable
-        className="items-center justify-center p-3"
-        onPress={handleChangeMode}
-      >
-        <Text className="text-blue-300">
-          {mode === 'register'
-            ? 'Already have an account? Log in.'
-            : 'Need an account? Register.'}
-        </Text>
-      </Pressable>
-    </View>
+    <SafeAreaView edges={['bottom']} className="flex-1 justify-between">
+      <KeyboardAvoidingView behavior="padding" className="gap-3">
+        <View className="border-hairline border-neutral-900 p-3 rounded-md">
+          <TextInput
+            keyboardType="email-address"
+            onChangeText={setEmail}
+            placeholder="Email Address"
+            autoCapitalize="none"
+            autoCorrect={false}
+            style={{ fontSize: 16 }}
+          />
+        </View>
+        <View className="border-hairline border-neutral-900 p-3 rounded-md">
+          <TextInput
+            onChangeText={setPassword}
+            placeholder="Password"
+            secureTextEntry
+            autoCapitalize="none"
+            autoCorrect={false}
+            style={{ fontSize: 16 }}
+          />
+        </View>
+        <Pressable
+          className={`items-center justify-center p-3 rounded-md ${
+            !email || !password ? 'bg-neutral-300' : 'bg-amber-500'
+          }`}
+          disabled={!email || !password}
+          onPress={handleAuthenticate}
+        >
+          <Text className="font-bold text-base text-white uppercase">
+            {mode === 'login' ? 'Login' : 'Register'}
+          </Text>
+        </Pressable>
+      </KeyboardAvoidingView>
+      <AuthenticationModeToggle mode={mode} onToggle={handleToggleMode} />
+    </SafeAreaView>
+  );
+}
+
+type AuthenticationModeToggleProps = {
+  mode: AuthenticationMode;
+  onToggle?: (mode: AuthenticationMode) => void;
+};
+
+function AuthenticationModeToggle({
+  mode,
+  onToggle,
+}: AuthenticationModeToggleProps) {
+  const handleChangeMode = useCallback(() => {
+    onToggle?.(mode === 'login' ? 'register' : 'login');
+  }, [mode, onToggle]);
+
+  let content: ReactNode;
+
+  if (mode === 'login') {
+    content = (
+      <>
+        <Text className="text-slate-400">Need an account? </Text>
+        <Text className="text-amber-500">Register.</Text>
+      </>
+    );
+  } else {
+    content = (
+      <>
+        <Text className="text-slate-400">Already have an account? </Text>
+        <Text className="text-amber-500">Login.</Text>
+      </>
+    );
+  }
+
+  return (
+    <Pressable
+      className="items-center justify-center p-3"
+      onPress={handleChangeMode}
+    >
+      <Text className="font-medium text-base text-slate-400">{content}</Text>
+    </Pressable>
   );
 }
 // function LoginScreen() {
--- libs/ghostbot/feature-admin/src/screens/login-screen.tsx
+++ libs/ghostbot/feature-admin/src/screens/login-screen.tsx
@@ -1,4 +1,5 @@
-import { View } from 'react-native';
+import { Text, View } from 'react-native';
+import { Image } from 'expo-image';
 import {
   AuthenticationCallback,
   AuthenticationForm,
@@ -6,6 +7,7 @@ import {
 } from '../components/authentication-form';
 import { useApp } from '@realm/react';
 import { useCallback, useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
 
 export function LoginScreen() {
   const app = useApp();
@@ -25,12 +27,29 @@ export function LoginScreen() {
   );
 
   return (
-    <View className="flex-1 justify-center">
-      <AuthenticationForm
-        mode={mode}
-        onAuthenticate={handleAuthenticate}
-        onChangeMode={handleChangeMode}
-      />
-    </View>
+    <>
+      <StatusBar style="light" animated />
+
+      <Image
+        contentFit="cover"
+        source={require('../../assets/background1.jpeg')}
+      >
+        <View className="flex">
+          <View className="basis-1/3 items-start justify-end p-6">
+            <Text className="text-5xl text-neutral-50 font-medium">Login</Text>
+            <Text className="text-base text-neutral-200">
+              Sign in to your account.
+            </Text>
+          </View>
+          <View className="basis-2/3 bg-white rounded-t justify-center p-6">
+            <AuthenticationForm
+              mode={mode}
+              onAuthenticate={handleAuthenticate}
+              onToggleMode={handleChangeMode}
+            />
+          </View>
+        </View>
+      </Image>
+    </>
   );
 }
