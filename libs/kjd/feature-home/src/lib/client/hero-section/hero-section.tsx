0b14c3a8d11a6f7d945901ef611f56eb9f749a71
Kennie Davis <kenniejaydavis@gmail.com>
2023-08-21 12:44:32 -0700

    feat(kjd): hero styling

--- libs/kjd/feature-home/src/lib/client/hero-section/hero-section.tsx
+++ libs/kjd/feature-home/src/lib/client/hero-section/hero-section.tsx
@@ -12,7 +12,7 @@ export function HeroSection({
 }: HeroSectionProps) {
   return (
     <Hero>
-      <Hero.Content className="flex flex-row text-neutral-300">
+      <Hero.Content className="flex flex-col md:flex-row text-neutral-300">
         <div className="flex flex-col basis-3/4 justify-center gap-8">
           <section>
             <h1 className="text-4xl font-serif">{title}</h1>
@@ -24,7 +24,7 @@ export function HeroSection({
             ))}
           </section>
         </div>
-        <div className="flex basis-1/4 items-center">
+        <div className="flex basis-1/4 items-start">
           <img alt={title} src={image?.url} />
         </div>
       </Hero.Content>
--- libs/kjd/ui-layout/src/lib/hero/hero-content.tsx
+++ libs/kjd/ui-layout/src/lib/hero/hero-content.tsx
@@ -7,7 +7,7 @@ export function HeroContentFn({
 }: HeroContentProps) {
   return (
     <div
-      className={`p-4 md:p-8 lg:p-16 max-w-5xl mx-auto ${className}`}
+      className={`p-4 md:px-0 md:py-8 lg:py-16 max-w-prose mx-auto ${className}`}
       {...rest}
     >
       {children}
