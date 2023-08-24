bf1486d80c299bb5b180988c851fb76b20d70ff5
Kennie Davis <kenniejaydavis@gmail.com>
2023-08-23 19:55:58 -0700

    feat(kjd): style hero section

--- libs/kjd/ui-pages/src/lib/client/hero-section/hero-section.tsx
+++ libs/kjd/ui-pages/src/lib/client/hero-section/hero-section.tsx
@@ -13,14 +13,24 @@ export function HeroSection({ fragment }: HeroSectionProps) {
   );
 
   return (
-    <section>
-      <h1>{title}</h1>
-      <p>{subtitle}</p>
-      <img alt={title ?? ''} src={image?.url} />
-      {content.map(({ raw }) => (
-        <RichText content={raw} />
-      ))}
-    </section>
+    <div className="bg-neutral-800">
+      <section className="grid grid-flow-row-dense grid-cols-3 grid-rows-2 max-w-5xl mx-auto p-4 text-white gap-y-4 md:gap-y-6 md:p-8 lg:p-16">
+        <section className="col-span-3 flex flex-col justify-center gap-y-2 md:col-span-2">
+          <h1 className="text-5xl font-serif font-medium md:text-5xl">
+            {title}
+          </h1>
+          <p className="text-base text-neutral-400">{subtitle}</p>
+        </section>
+        <section className="hidden row-span-2 md:flex">
+          <img alt={title ?? ''} src={image?.url} />
+        </section>
+        <section className=" flex flex-col gap-y-1 col-span-3 text-sm md:col-span-2 lg:text-base">
+          {content.map(({ raw }) => (
+            <RichText content={raw} />
+          ))}
+        </section>
+      </section>
+    </div>
   );
 }
 
