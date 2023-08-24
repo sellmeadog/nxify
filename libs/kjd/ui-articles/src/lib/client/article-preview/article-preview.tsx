3b5c830017347efc252714a1da77ac6dfe799a52
Kennie Davis <kenniejaydavis@gmail.com>
2023-08-23 20:11:29 -0700

    feat(kjd): style article preview

--- libs/kjd/ui-articles/src/lib/client/article-preview/article-preview.tsx
+++ libs/kjd/ui-articles/src/lib/client/article-preview/article-preview.tsx
@@ -1,5 +1,7 @@
+import { format, parseISO } from 'date-fns';
 import { FragmentType, useFragment } from '../../generated';
 import { ArticlePreviewFragmentDoc } from '../../generated/graphql';
+import Link from 'next/link';
 
 /* eslint-disable-next-line */
 export interface ArticlePreviewProps {
@@ -7,14 +9,25 @@ export interface ArticlePreviewProps {
 }
 
 export function ArticlePreview({ fragment }: ArticlePreviewProps) {
-  const { excerpt, title } = useFragment(ArticlePreviewFragmentDoc, fragment);
+  const { createdAt, excerpt, title } = useFragment(
+    ArticlePreviewFragmentDoc,
+    fragment
+  );
+
   return (
-    <article className="text-white">
-      <header>
-        <h1>{title}</h1>
+    <article className="flex flex-col border-b border-neutral-800 last-of-type:border-transparent gap-y-4 max-w-5xl mx-auto p-4 text-neutral-200 md:p-8 lg:p-16">
+      <header className="flex flex-col gap-y-px">
+        <Link href={'/'}>
+          <h2 className="text-cyan-400 text-lg md:text-2xl lg:text-3xl hover:text-cyan-300">
+            {title}
+          </h2>
+        </Link>
+        <p className="text-neutral-400 text-sm uppercase">
+          {format(parseISO(createdAt), 'PPP')}
+        </p>
       </header>
       <main>
-        <p>{excerpt}</p>
+        <p className="text-base">{excerpt}</p>
       </main>
     </article>
   );
