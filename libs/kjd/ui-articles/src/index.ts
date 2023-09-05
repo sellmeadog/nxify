3dbbc3ca3f0a0c01ec3b702bef7085fb67cfe7d1
Kennie Davis <kenniejaydavis@gmail.com>
2023-09-05 15:09:47 -0700

    chore(kjd): remove deprecated libs

--- libs/kjd/ui-articles/src/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './lib/client/article-body/article-body';
-// Use this file to export React client components (e.g. those with 'use client' directive) or other non-server utilities
-export * from './lib/client/article-hero/article-hero';
-export * from './lib/client/article-preview-list/article-preview-list';
